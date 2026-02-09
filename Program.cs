df5578883039c9ec6acdbfaf5cb2e1566ae9219e
sirajhasand <sirajuddinhasand@gmail.com>
2026-02-09 19:56:51 +0430

    main branch updated

--- Program.cs
+++ Program.cs
@@ -6,25 +6,6 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("mian branch");
-        Console.WriteLine("You can write your C# code here and run it using the .NET CLI.");
-        int x = 10;
-        int y = 20;
-        int sum = x + y;
-        Console.WriteLine($"The sum of {x} and {y} is: {sum}"); 
-
-        double a = 5.5;
-        double b = 2.3; 
-        double product = a * b;
-        Console.WriteLine($"The product of {a} and {b} is: {product }");
-        string jobTitle = "Software Developer";
-        Console.WriteLine($"I am a {jobTitle}.");
-    
-        int sum = x - y;
-        Console.WriteLine($"The difference of {x} and {y} is: {sum}"); 
-        string name = "Alice";
-        Console.WriteLine($"Hello, {name}!");
-        string greeting = "Welcome to C# programming!";
-        Console.WriteLine(greeting); 
+        Console.WriteLine("main branch");
     }
 }
