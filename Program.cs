65e2cf4c28ed5c87aac120732bf40fefa50fa7a2
sirajhasand <sirajuddinhasand@gmail.com>
2026-02-09 10:57:04 +0430

    new changes

--- Program.cs
+++ Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello from VS Code!");
+        Console.WriteLine("This is a simple C# program running in VS Code.");
     }
 }
