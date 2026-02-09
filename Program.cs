20d7c30472f3a874cc407b8f15bf6a03bb0fb8a6
sirajhasand <sirajuddinhasand@gmail.com>
2026-02-09 11:12:49 +0430

    sum to diffrence

--- Program.cs
+++ Program.cs
@@ -4,12 +4,11 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello from VS Code!");
-        Console.WriteLine("This is a simple C# program running in VS Code.");
+        Console.WriteLine("test branch");
         Console.WriteLine("You can write your C# code here and run it using the .NET CLI.");
         int x = 10;
         int y = 20;
-        int sum = x + y;
-        Console.WriteLine($"The sum of {x} and {y} is: {sum}"); 
+        int sum = x - y;
+        Console.WriteLine($"The difference of {x} and {y} is: {sum}"); 
     }
 }
