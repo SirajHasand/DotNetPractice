359ecc7f29024396f1bdffeeedbd0b7e8d82f315
sirajhasand <sirajuddinhasand@gmail.com>
2026-02-09 11:09:46 +0430

    new commit

--- Program.cs
+++ Program.cs
@@ -6,5 +6,10 @@ class Program
     {
         Console.WriteLine("Hello from VS Code!");
         Console.WriteLine("This is a simple C# program running in VS Code.");
+        Console.WriteLine("You can write your C# code here and run it using the .NET CLI.");
+        int x = 10;
+        int y = 20;
+        int sum = x + y;
+        Console.WriteLine($"The sum of {x} and {y} is: {sum}"); 
     }
 }
